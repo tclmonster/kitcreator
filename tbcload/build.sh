c6dc8a1adbc3dc8bcab85d9eb77a0d02f83a9086
Bandoti <Bandoti@noemail.net>
2024-01-05 19:34:36 +0000

    Add tbcload extension
    FossilOrigin-Name: 827cb2fbb7084710dae431c593e709be5aa42a1e

---
+++ tbcload/build.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# BuildCompatible: KitCreator
+
+version='1.7'
+url="https://github.com/bandoti/tbcload/archive/refs/tags/v${version}.tar.gz"
+sha256='12de0a9b6e777a42f4037c8b4a59e7613f8034cd43b8a4475dedfce82d728fd6'
