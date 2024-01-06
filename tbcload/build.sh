7c1e856b4b662feb2b4094c960e75f79a83663a4
Bandoti <mason@bandoti.com>
2024-01-06 00:19:48 +0000

    Copy tbcload public headers into KitDLL
    FossilOrigin-Name: 39c9a108cf2b5894b54a240d24b854ef9b78c687

--- tbcload/build.sh
+++ tbcload/build.sh
@@ -5,3 +5,7 @@
 version='1.7'
 url="https://github.com/bandoti/tbcload/archive/refs/tags/v${version}.tar.gz"
 sha256='12de0a9b6e777a42f4037c8b4a59e7613f8034cd43b8a4475dedfce82d728fd6'
+
+function postinstall() {
+	cp -r "${installdir}/include" "${runtimedir}" || return 1
+}
