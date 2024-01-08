329a061c762bd67854ec2838bfb680d40db0f085
Bandoti <mason@bandoti.com>
2024-01-08 19:04:09 +0000

    Upgrade tcllib version
    FossilOrigin-Name: 5cb47ae89543ac3d748d9130468dca1d0657463f

--- tcllib/build.sh
+++ tcllib/build.sh
@@ -2,6 +2,6 @@
 
 # BuildCompatible: KitCreator
 
-version='1.20'
+version='1.21'
 url="http://sourceforge.net/projects/tcllib/files/tcllib/${version}/tcllib-${version}.tar.bz2"
-sha256='3a33f212f35235f9dca4425c5c5692186515be169b0b6e3ca498e7c344ea83b8'
+sha256='4c8a5fc12896002a700dbc6ba3d7c7806a48fb8b89b60a3e902c9098a31757be'
