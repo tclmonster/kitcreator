d85b5faf860e32431efb3734a474d01f005b34ab
Bandoti <Bandoti@noemail.net>
2024-01-12 15:25:49 +0000

    tbcload: only install headers in kitdll
    FossilOrigin-Name: f57c8f9b1e8db48959cdc591b92c0ed246e6e9c4

--- tbcload/build.sh
+++ tbcload/build.sh
@@ -7,5 +7,7 @@ url="https://github.com/bandoti/tbcload/archive/refs/tags/v${version}.tar.gz"
 sha256='12de0a9b6e777a42f4037c8b4a59e7613f8034cd43b8a4475dedfce82d728fd6'
 
 function postinstall() {
-	cp -r "${installdir}/include" "${runtimedir}" || return 1
+	if [ "$KITTARGET" = "kitdll" ]; then
+		cp -r "${installdir}/include" "${runtimedir}" || return 1
+	fi
 }
