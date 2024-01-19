782272e09621471fc7ecb42aefa393de58b9d76a
Bandoti <mason@bandoti.com>
2024-01-19 17:50:30 +0000

    Update tclparser commit hash
    FossilOrigin-Name: 62abf64c8261c261d08e68a059058e8186176d20

--- tclparser/build.sh
+++ tclparser/build.sh
@@ -4,6 +4,6 @@
 
 version='1.4'
 url="https://github.com/bandoti/tclparser/archive/refs/tags/v${version}.tar.gz"
-sha256='d30cc1f93945f8f962214b7b76ff9ea9016285227b2fbd0293c7a90489e0eedd'
+sha256='29c2fdb1db7e9b29dc357364a74ff78f14dcf627ed662643b15f8fbe1e991a9b'
 
 configure_extra=(--with-tclinclude=${KITCREATOR_DIR}/tcl/inst/include)
