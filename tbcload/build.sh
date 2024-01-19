1ce0294089f09e98b24f55e77a7ffa8588141545
Bandoti <mason@bandoti.com>
2024-01-19 21:23:30 +0000

    Allow implicit function decls for tbcload et al
    FossilOrigin-Name: c88f16738350c013ccb8034598d144a1cb910260

--- tbcload/build.sh
+++ tbcload/build.sh
@@ -6,6 +6,8 @@ version='1.7'
 url="https://github.com/bandoti/tbcload/archive/refs/tags/v${version}.tar.gz"
 sha256='a1cbfdb0e3e61e08b996df713dd210a6103a7828650aa39630355cecd1c24b63'
 
+KC_TBCLOAD_CFLAGS='-Wno-error=implicit-function-declaration'
+
 function postinstall() {
 	if [ "$KITTARGET" = "kitdll" ]; then
 		cp -r "${installdir}/include" "${runtimedir}" || return 1
