4a6592c728a593d69c9d931f65346ca1ff8cf079
Bandoti <mason@bandoti.com>
2024-01-19 17:09:18 +0000

    Update tbcload commit hash
    FossilOrigin-Name: e3f8b09eda42b7b574e9a6f486b9b35974075f8b

--- tbcload/build.sh
+++ tbcload/build.sh
@@ -4,7 +4,7 @@
 
 version='1.7'
 url="https://github.com/bandoti/tbcload/archive/refs/tags/v${version}.tar.gz"
-sha256='12de0a9b6e777a42f4037c8b4a59e7613f8034cd43b8a4475dedfce82d728fd6'
+sha256='a1cbfdb0e3e61e08b996df713dd210a6103a7828650aa39630355cecd1c24b63'
 
 function postinstall() {
 	if [ "$KITTARGET" = "kitdll" ]; then
