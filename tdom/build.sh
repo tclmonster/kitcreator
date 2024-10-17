230221c034cb895b6fb282361512383d6cbfbc56
Bandoti <Bandoti@noemail.net>
2024-10-17 18:43:35 +0000

    Upgrade tdom to 0.9.5
    FossilOrigin-Name: dc5b3558871589ce629f47c2bcf2803185360da6

--- tdom/build.sh
+++ tdom/build.sh
@@ -2,6 +2,6 @@
 
 # BuildCompatible: KitCreator
 
-version='0.9.1'
+version='0.9.5'
 url="http://tdom.org/downloads/tdom-${version}-src.tgz"
-sha256='3b1f644cf07533fe4afaa8cb709cb00a899d9e9ebfa66f4674aa2dcfb398242c'
+sha256='ce22e3f42da9f89718688bf413b82fbf079b40252ba4dd7f2a0e752232bb67e8'
