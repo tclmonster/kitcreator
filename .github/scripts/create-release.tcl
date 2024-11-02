cc0c235aa4e175d75baa032a87fa9bd4b9b6c7e0
Bandoti <Bandoti@noemail.net>
2024-11-02 02:10:40 +0000

    Format JSON strings
    FossilOrigin-Name: 9e3767985d15e239345250d29e07e2af0be892cd

--- .github/scripts/create-release.tcl
+++ .github/scripts/create-release.tcl
@@ -24,14 +24,19 @@ lassign $argv api_url owner repo tag_name
 
 set release_url $api_url/repos/$owner/$repo/releases
 set headers [list Accept application/vnd.github+json Authorization "Bearer $env(GH_TOKEN)" X-GitHub-Api-Version 2022-11-28]
-set req_data [::tcl::chan::string [::json::write object tag_name $tag_name]]
+set req_data [::tcl::chan::string [::json::write object-strings tag_name $tag_name]]
 set response [http::geturl $release_url -type application/json -querychannel $req_data -headers $headers]
 if {[http::status $response] != "ok"} {
-    puts stderr "Failed to create release at \"$release_url\" using tag \"$tag_name\""
+    puts stderr "Error retrieving URL \"$release_url\""
+    http::cleanup $response
     exit 1
 }
 
-puts "[http::data $response]"
+if {[http::ncode $response] != 201} {
+    puts stderr "Failed to create release"
+    puts stderr [http::data $response]
+    exit 1
+}
 
 set upload_url [string map [list \{?name,label\} {}] \
                     [dict get [::json::json2dict [http::data $response]] upload_url]]
