7a976a543c00d38335767de7dcdfe1c294e2432f
Bandoti <mason@bandoti.com>
2024-11-02 13:25:21 +0000

    Fix queries
    FossilOrigin-Name: 3f5148cf2968ba257ed8106d460dd5272cbbbdbf

--- .github/scripts/create-release.tcl
+++ .github/scripts/create-release.tcl
@@ -6,7 +6,6 @@ package require http
 package require tls
 package require json
 package require json::write
-package require tcl::chan::string
 
 http::register https 443 [list ::tls::socket -autoservername true -require true]
 
@@ -23,30 +22,32 @@ if {[llength $argv] < 4} {
 lassign $argv api_url owner repo tag_name
 
 set release_url $api_url/repos/$owner/$repo/releases
-set headers [list Accept application/vnd.github+json Authorization "Bearer $env(GH_TOKEN)" X-GitHub-Api-Version 2022-11-28]
-set req_data [::tcl::chan::string [::json::write object-strings tag_name $tag_name]]
-set response [http::geturl $release_url -type application/json -querychannel $req_data -headers $headers]
+set headers  [list Accept application/vnd.github+json Authorization "Bearer $env(GH_TOKEN)" X-GitHub-Api-Version 2022-11-28]
+set response [http::geturl $release_url -headers $headers \
+                  -type application/json \
+                  -query [::json::write object-strings tag_name $tag_name]]
+
 if {[http::status $response] != "ok"} {
-    puts stderr "Error retrieving URL \"$release_url\""
-    http::cleanup $response
+    puts stderr "Error retrieving URL: \"[http::error $response]\""
     exit 1
 }
 
 if {[http::ncode $response] != 201} {
-    puts stderr "Failed to create release"
-    puts stderr [http::data $response]
+    puts stderr "Failed to create release: \"[http::data $response]\""
     exit 1
 }
 
 set upload_url [string map [list \{?name,label\} {}] \
                     [dict get [::json::json2dict [http::data $response]] upload_url]]
 
+http::cleanup $response
+
 foreach kitfile [glob kitcreator-*-kits/*] {
     puts -nonewline "Uploading \"$kitfile\"... "
     set query [http::formatQuery name [file tail $kitfile]]
     set kitchan  [open $kitfile]
     fconfigure $kitchan -translation binary
-    set response [http::geturl $upload_url -query $query -querychannel $kitchan -type application/octet-stream -headers $headers]
+    set response [http::geturl ${upload_url}?${query} -headers $headers -querychannel $kitchan -type application/octet-stream]
     if {[http::status $response] != "ok" || [http::ncode $response] != 201} {
         puts "Failed!"
         puts stderr [http::data $response]
