28814d176a8fccd2d240e91330c732dcc392ea50
Bandoti <Bandoti@noemail.net>
2024-11-03 13:49:10 +0000

    Populate release name/body fields
    FossilOrigin-Name: 6b5be6834e0a261bdbde28d927bbad72cbc7d124

--- .github/scripts/create-release.tcl
+++ .github/scripts/create-release.tcl
@@ -5,7 +5,6 @@
 package require http
 package require tls
 package require json
-package require json::write
 
 http::register https 443 [list ::tls::socket -require true]
 
@@ -21,11 +20,33 @@ if {[llength $argv] < 4} {
 
 lassign $argv api_url owner repo tag_name
 
+set release_body {
+    Tcl kits available for Linux, macOS, and Windows.
+
+    The \"tclkitsh\" kits are command-line only; \"tclkit\" kits contain Tk as a shared library and
+    so they may include a GUI if desired; \"libtclkit-sdk\" archives may be extracted and used to link
+    extensions against (without having to compile Tcl/Tk), or they may be linked into an application
+    to provide a static Tcl/Tk interpreter. Kits with the \"minimal\" suffix only contain base Tcl/Tk
+    libraries.
+}
+
+set release_query {
+    {
+        "tag_name": "_TAG_",
+        "target_commitish": "main",
+        "name": "_NAME_",
+        "body": "_BODY_"
+    }
+}
+set release_query [string map [list _TAG_  $tag_name \
+                                    _NAME_ "Monster kits ([clock format [clock seconds] -format {%Y-%m-%d}])" \
+                                    _BODY_ [regsub -all {\n} $release_body {\\n}]] $release_query]
+
 set release_url $api_url/repos/$owner/$repo/releases
 set headers  [list Accept application/vnd.github+json Authorization "Bearer $env(GH_TOKEN)" X-GitHub-Api-Version 2022-11-28]
 set response [http::geturl $release_url -keepalive true -headers $headers \
                   -type application/json \
-                  -query [::json::write object-strings tag_name $tag_name]]
+                  -query $release_query]
 
 if {[http::status $response] != "ok"} {
     puts stderr "Error retrieving URL: \"[http::error $response]\""
@@ -43,11 +64,13 @@ set upload_url [string map [list \{?name,label\} {}] \
 foreach kitfile [glob kitcreator-*-kits/*] {
     puts -nonewline "Uploading \"$kitfile\"... "
     set query [http::formatQuery name [file tail $kitfile]]
-    set kitchan  [open $kitfile]
+    set kitchan [open $kitfile]
     fconfigure $kitchan -translation binary
 
     set response [http::geturl ${upload_url}?${query} -keepalive true -headers $headers \
-                      -querychannel $kitchan -type application/octet-stream]
+                      -type application/octet-stream \
+                      -querychannel $kitchan]
+
     if {[http::status $response] != "ok" || [http::ncode $response] != 201} {
         puts "Failed!"
         puts stderr [http::data $response]
