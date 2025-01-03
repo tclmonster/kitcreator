553bf0a38e9f1c885f5f28a19d632fd0cb79d17d
Bandoti <Bandoti@noemail.net>
2025-01-03 17:38:30 +0000

    Fix zlibtcl download URL
    FossilOrigin-Name: a3bab38cb4950fd2b11e2b1426e3f07766961a8a

--- zlibtcl/build.sh
+++ zlibtcl/build.sh
@@ -3,7 +3,7 @@
 # BuildCompatible: KitCreator
 
 version='2.0.1'
-url="https://download.sourceforge.net/project/tkimg/tkimg/2.0/tkimg%20${version}/Img-${version}-Source.tar.gz"
+url="https://download.sourceforge.net/project/tkimg/tkimg/2.0/tkimg%20${version}/Img-${version}.tar.gz"
 sha256='e69d31b3f439a19071e3508a798b9d5dc70b9416e00926cdac12c1c2d50fce83'
 
 # zlibtcl has different version than enclosing Img package
