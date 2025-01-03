32db6d73cddac54f4be5b6cd0a74fb5bd1887cd6
Bandoti <Bandoti@noemail.net>
2025-01-03 17:25:44 +0000

    Upgrade zlibtcl to 2.0.1
    FossilOrigin-Name: 1126efb1fc1d45530d112d3fb2843ee250e06fba

--- zlibtcl/build.sh
+++ zlibtcl/build.sh
@@ -2,12 +2,12 @@
 
 # BuildCompatible: KitCreator
 
-version='1.4.16'
-url="https://download.sourceforge.net/project/tkimg/tkimg/1.4/tkimg%20${version}/Img-${version}-Source.tar.gz"
-sha256='d99af4835fe3e20960817c7a1b5235dcfaa97c642593cce50bdb64c5827cd321'
+version='2.0.1'
+url="https://download.sourceforge.net/project/tkimg/tkimg/2.0/tkimg%20${version}/Img-${version}-Source.tar.gz"
+sha256='e69d31b3f439a19071e3508a798b9d5dc70b9416e00926cdac12c1c2d50fce83'
 
 # zlibtcl has different version than enclosing Img package
-tclpkgversion='1.2.13'
+tclpkgversion='1.3.1'
 
 function preconfigure() {
 	# Only interested in compiling zlibtcl sub-project
