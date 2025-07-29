b66643bb162f8a2d49ad36a8373ea6aee5907d23
Bandoti <mason@bandoti.com>
2025-07-29 14:07:44 +0000

    Fix static TWAPI support
    FossilOrigin-Name: 5706d785f46c1764087bf13fda5ba2d169dea6bd

--- twapi/build.sh
+++ twapi/build.sh
@@ -7,10 +7,15 @@ url="https://github.com/apnadkarni/twapi/archive/refs/tags/v${version}.tar.gz"
 sha256='1ad8bf29c419e684bb653e5b11556f0cf74ab6d78bd59d5345d5bf550d6e3363'
 
 function postinstall() {
-    # TODO: add the dyncall dependency for static builds. At the
-    # moment it is currently missing, so the static build won't work.
 
     find "${installdir}" -type f -name '*.a' | while IFS='' read -r filename; do
-        echo '-lole32 -loleaut32 -lwtsapi32 -lcrypt32 -lwintrust -luuid -lsetupapi -lcfgmgr32 -ltdh -lwinmm -liphlpapi -lpsapi -lpowrprof -lpdh -lshlwapi -lversion -lgdi32 -lsecur32 -lcredui -lmpr -luxtheme -lrpcrt4 -lwinspool' > "${filename}".linkadd
+
+        # Makefile dyncall lib path is relative to the workdir
+        twapi_libs="$(grep '^LIBS[[:space:]]*=' "${workdir}/Makefile" |
+                              sed -e 's@^LIBS[[:space:]]*=[[:space:]]*@@' \
+                                  -e "s@\\./dyncall@${workdir}/dyncall@g" |
+                                      tr -d '\t')"
+
+        echo "$twapi_libs" > "${filename}".linkadd
     done
 }
