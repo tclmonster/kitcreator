30b8670930e60ea5c8268cbb5d6439d93e80023f
Bandoti <mason@bandoti.com>
2026-03-29 21:19:25 +0000

    Upgrade tclvfs to 1.5.0
    FossilOrigin-Name: 7a9b3d4085bba1678972b2c3200217ac4f1219eb

--- tclvfs/build.sh
+++ tclvfs/build.sh
@@ -2,27 +2,10 @@
 
 # BuildCompatible: KitCreator
 
-version="20080503"
-url="http://sourceforge.net/projects/tclvfs/files/tclvfs/tclvfs-${version}/tclvfs-${version}.tar.gz"
-sha256='0d90362078c8f59347b14be377e9306336b6d25d147397f845e705a6fa1d38f2'
+version="1.5.0"
+url="https://core.tcl-lang.org/tclvfs/tarball/tclvfs-20251211172729-77037fcb2c.tar.gz"
+sha256='4788f023cc3f850820f4c8a662783ea7d6dceccecf7df247dabf4435e9201fbc'
 
 function init_kitcreator() {
 	pkg_always_static='1'
-}
-
-function preconfigure() {
-	local buildtype
-
-	cp generic/vfs.c .
-
-	# If we are building for Win32, we need to define TEA_PLATFORM so that
-	# the right private directory is found
-	buildtype="$(basename "${TCLCONFIGDIR}")"
-	if [ "${buildtype}" = "win" ]; then
-		TEA_PLATFORM="windows"
-		export TEA_PLATFORM
-
-		CFLAGS="${CFLAGS} -I${TCLCONFIGDIR}"
-		export CFLAGS
-	fi
 }
