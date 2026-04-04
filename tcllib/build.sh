59cc6aea150df1a6a838d5e50693f9189f30eb22
Bandoti <mason@bandoti.com>
2026-04-04 22:05:39 +0000

    tcllib: use install-libraries target
    FossilOrigin-Name: c5def0102ec1299e3b18da27369b98fdbabaedd6

--- tcllib/build.sh
+++ tcllib/build.sh
@@ -5,3 +5,4 @@
 version='2.0'
 url="http://sourceforge.net/projects/tcllib/files/tcllib/${version}/tcllib-${version}.tar.bz2"
 sha256='196c574da9218cf8dcf180f38a603e670775ddb29f191960d6f6f13f52e56b04'
+make_extra=(install-libraries)
