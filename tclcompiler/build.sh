4b391243e808bf3db9aa4e4cb17a240821902d98
Bandoti <mason@bandoti.com>
2026-04-05 16:02:37 +0000

    Upgrade tclcompiler to version 2.0a0
    FossilOrigin-Name: 77a9c010c79c4bdf11fb8f4297e543dc53afaecd

--- tclcompiler/build.sh
+++ tclcompiler/build.sh
@@ -2,22 +2,9 @@
 
 # BuildCompatible: KitCreator
 
-version='1.7.1'
-url="https://github.com/bandoti/tclcompiler/archive/refs/tags/v${version}.tar.gz"
-sha256='6d44b25707f117a900b80662b685c702c5ea59c773060707227385115fdcb421'
+version='2.0a0'
+url="https://github.com/tclmonster/tclcompiler2/archive/refs/tags/v${version}.tar.gz"
+sha256='f6669224a677d6bdc43e0c3ce1854d8cfd61ece561250d1e5d94e97085ccd627'
 
 KC_TCLCOMPILER_CFLAGS='-Wno-error=implicit-function-declaration'
 configure_extra=(--with-tclinclude=${KITCREATOR_DIR}/tcl/inst/include)
-
-function prebuild() {
-	local tbcload_headers="$(ls ${KITCREATOR_DIR}/tbcload/inst/include/*.h 2>/dev/null)"
-
-	if [ -z "$tbcload_headers" ]; then
-		echo "Error: tbcload must be built prior to tclcompiler"
-		return 1
-	fi
-
-	for h in $tbcload_headers; do
-		cp $h $workdir || return 1
-	done
-}
