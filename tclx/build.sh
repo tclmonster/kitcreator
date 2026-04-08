59d4e4b110f039225545de7dfca926d6d88f7dbd
Bandoti <mason@bandoti.com>
2026-04-08 23:46:48 +0000

    Upgrade tclx to version 9.0 beta
    FossilOrigin-Name: 4bd88abc85bf9344dd3ee960e3a99284177a2590

--- tclx/build.sh
+++ tclx/build.sh
@@ -2,8 +2,8 @@
 
 # BuildCompatible: KitCreator
 
-version="8.6.2"
-url="https://github.com/flightaware/tclx/archive/refs/tags/v${version}.tar.gz"
-sha256='157124fa98b324c0a57c5a9f4d709bedb00a16f43b0aa8263b1ba56005d30035'
+version="9.0.0"
+url="https://github.com/tclmonster/tclx/archive/refs/tags/v${version}-beta.tar.gz"
+sha256='6075ba010ec73151a3d33502e0162c002de43db9ce6ed7f9f00a8b381435b9ad'
 tclpkg='Tclx'
-tclpkgversion='8.6'
+tclpkgversion='9.0'
